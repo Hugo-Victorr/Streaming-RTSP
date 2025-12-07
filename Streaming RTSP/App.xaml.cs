f41fd379c6a2327de3b6dd5812950af55ffed7a8
Hugo Victor Oliveira lima <hugo.kun@hotmail.com>
2025-12-07 20:39:03 -0300

    feat: add wpf project

---
+++ Streaming RTSP/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Prism.DryIoc;
+using Prism.Events;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
+using Streaming_RTSP.Views;
+using System.Windows;
+
+namespace Streaming_RTSP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterSingleton<INLogService, NLogService>();
+            //containerRegistry.RegisterSingleton<IEventAggregator, EventAggregator>();
+        }
+    }
+}
---
+++ Streaming RTSP/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Streaming_RTSP.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
